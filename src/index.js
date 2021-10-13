3732805e005bc7440c792375aa04611c72937461
Phathdt <phathdt379@gmail.com>
2021-10-13 11:35:32 +0700

    init

---
+++ src/index.js
@@ -0,0 +1,77 @@
+import cors from 'cors'
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+import multer from 'multer'
+import path from 'path'
+import fs from 'fs'
+import puppeteer from 'puppeteer'
+import { customAlphabet } from 'nanoid'
+const nanoid = customAlphabet('1234567890abcdef', 10)
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${nanoid()}-${file.originalname}`)
+  },
+})
+
+const upload = multer({
+  limits: {
+    fileSize: 10 * 1024 * 1024,
+  },
+  storage: storage,
+})
+
+const app = express()
+
+app.use(cors())
+
+app.get('/ping', (_, res) => {
+  res.json({ msg: 'pong' })
+})
+
+const convert = async ({ file }, res, next) => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+      '--disable-setuid-sandbox',
+      '--no-sandbox',
+    ],
+  })
+
+  const page = await browser.newPage()
+
+  const content = await fs.readFileSync(
+    `${path.join(__dirname, `/../${file.path}`)}`,
+    { encoding: 'utf8', flag: 'r' }
+  )
+
+  await page.setContent(content)
+
+  const pdf = await page.pdf({
+    format: 'A5',
+    margin: {
+      top: '0px',
+      left: '0px',
+      right: '0px',
+      bottom: '0px',
+    },
+  })
+
+  browser.close()
+
+  await fs.promises.unlink(`${path.join(__dirname, `/../${file.path}`)}`)
+
+  res.contentType('application/pdf')
+  res.send(pdf)
+}
+
+app.post('/convert', asyncHandler(upload.single('file')), asyncHandler(convert))
+
+const PORT = 5000
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
